c324f6cfe5c104b19ba170672722b72c9712ab53
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-20 14:14:27 +0300

    Tasl 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const gallery = galleryItems
+  .map(
+    (image) =>
+      `<ul>
+            <li class="gallery__item">
+                <img class="gallery__image" src="${image.preview}" alt="${image.description}" srcset="${image.original}"/>
+            </li>
+        </ul>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", gallery);
+
+galleryContainer.addEventListener("click", bigPicture);
+function bigPicture(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.srcset}">
+`);
+
+  instance.show();
+}
